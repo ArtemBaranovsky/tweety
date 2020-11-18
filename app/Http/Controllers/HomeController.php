0c66c9875f0b6399179d8c6fe25fe8939274bc95
Artem Baranovsky <artem.baranovsky1980@gmail.com>
2020-11-18 21:12:23 +0200

    57 Make the Timeline Dynamic

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+//use App\Tweet;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,11 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+//        $tweets = Tweet::latest()->get();
+
+        return view('home', [
+//            'tweets' => $tweets
+            'tweets' => auth()->user()->timeline()
+        ]);
     }
 }
--- resources/views/_tweet.blade.php
+++ resources/views/_tweet.blade.php
@@ -1,14 +1,17 @@
 <div class="flex p-4 border-b border-b-grey-400">
     <div class="mr-2 flex-shrink-0">
-        <img src="https://i.pravatar.cc/50"
+        <img
+            src="{{ $tweet->user->avatar }}"
+{{--            src="https://i.pravatar.cc/50?u={{ $tweet->user->email }}"--}}
              alt=""
              class="rounded-full mr-2"
         >
     </div>
+
     <div>
-        <h5 class="font-bold mb-4">John Doe</h5>
+        <h5 class="font-bold mb-4">{{ $tweet->user->name }}</h5>
         <p class="text-sm">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi ea ipsa ipsam molestiae officiis voluptate voluptates. Aspernatur dolor dolores esse harum in libero modi quis.
+            {{ $tweet->body }}
         </p>
     </div>
 </div>
